7d8b23225bdac4bdb0d88a6b875beb3406340ad7
Jean Janrley <jeanjanrley@outlook.com>
2022-08-04 18:03:52 -0300

    Exugando codigos em excesso

---
+++ config.js
@@ -0,0 +1,7 @@
+const nav = document.getElementById("nav-box");
+const navContainer = document.getElementById("nav-container");
+
+const menu = document.getElementById("menu-area");
+const logoAndMenu = document.getElementById("logo-and-menu");
+
+const closeButton = document.getElementById("close-button");
